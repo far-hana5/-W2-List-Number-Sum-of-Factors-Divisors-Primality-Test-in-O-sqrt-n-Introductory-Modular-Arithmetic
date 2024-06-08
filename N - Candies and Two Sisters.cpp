670c29bfdcb0256efe48f795f241ba1d9ccc452d
farhana akter <119425449+far-hana5@users.noreply.github.com>
2024-06-08 13:21:59 +0600

    Add files via upload

---
+++ N - Candies and Two Sisters.cpp
@@ -0,0 +1,16 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+int t;cin>>t;
+while(t--){
+   ll n;
+   cin>>n;
+   if(n%2==0) {
+    cout<<(n/2)-1<<endl;
+   }else{
+   cout<<n/2<<endl;
+   }
+}
+}
