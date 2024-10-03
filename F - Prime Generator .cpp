4afe053a602ffcded3ab8c02b8c43eb314cc1d2c
farhana akter <119425449+far-hana5@users.noreply.github.com>
2024-10-03 14:07:20 +0600

    Update F - Prime Generator .cpp

--- F - Prime Generator .cpp
+++ F - Prime Generator .cpp
@@ -1,1 +1,36 @@
 
+
+#include<iostream>
+#include<vector>
+typedef long long ll;
+using namespace std;
+
+bool prime[1000000000];
+
+
+bool isPrime(int num){
+if(num<2)return false;
+if(num==2)return true;
+if(num%2==0)return false;
+ for(int i=3;i*1ll*i<=num;i=i+2){
+    if(num%i==0)return false;
+ }
+ return true;
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+         ll n,m;
+    cin>>m>>n;
+   for(int i=m;i<=n;i++){
+    if(isPrime(i)){
+        cout<<i<<endl;
+    }
+   }
+
+    }
+
+return 0;
+}
+
