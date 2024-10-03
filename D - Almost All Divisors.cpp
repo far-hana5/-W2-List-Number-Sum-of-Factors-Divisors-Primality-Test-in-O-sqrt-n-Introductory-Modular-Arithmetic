c7b145586aeb06de2907c72d04408ffd215867cf
farhana akter <119425449+far-hana5@users.noreply.github.com>
2024-10-03 13:21:04 +0600

    Update D - Almost All Divisors.cpp

--- D - Almost All Divisors.cpp
+++ D - Almost All Divisors.cpp
@@ -1,18 +1,23 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 typedef long long ll;
 int answer(){
-int n;cin>>n;
-vector<int>arr(n);
-for(int i=0;i<n;i++){
-    cin>>arr[i];
-}
-sort(arr.begin(),arr.end());
+  int n;
+    cin>>n;
+    vector< int>arr(n);
 
-ll ans=(ll)arr[0]*arr[n-1];
-  vector< int>m;
-    for (int i = 2; i*1ll*i <= ans; i++){
-        if (ans % i == 0){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sort(arr.begin(),arr.end());
+    long long ans=(long long)arr[0] * arr[n-1];
+     vector< int>m;
+    for (int i = 2; i*1ll*i <= ans; i++)
+    {
+        if (ans % i == 0)
+        {
             m.push_back(i);
             if (i != ans / i)
                 m.push_back(ans / i);
@@ -24,13 +29,14 @@ if(arr==m){
 }else{
 cout<<-1<<endl;
 }
+return 0;
+}
+int main()
+{
+  int t;
+  cin>>t;
+  while(t--)  {
+   answer();
+  }
+}
 
-}
-int main(){
-int t;
-cin>>t;
-while(t--){
-    answer();
-}
-
-}
